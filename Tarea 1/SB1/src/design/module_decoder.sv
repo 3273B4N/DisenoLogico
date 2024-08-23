57eeff4cb1d9edef3ffb581b74d5bb5820ca0a9a
3273B4N <140739690+3273B4N@users.noreply.github.com>
2024-08-22 20:49:13 -0600

    Sub circuito 1

---
+++ Tarea 1/SB1/src/design/module_decoder.sv
@@ -0,0 +1,7 @@
+module decoder (input logic ag, bg, cg, dg, output logic ab, bb, cb, db);
+    assign ab = ag;
+    assign bb = (ag ^ bg);
+    assign cb = ((ag ^ bg) ^ cg);
+    assign db = (((ag ^ bg) ^ cg) ^ dg);
+endmodule
+
